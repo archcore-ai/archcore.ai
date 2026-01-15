48fcb44f47f0e1db0931e282433dcbb63be729e9
ivklgn <ivklgn@proton.me>
2026-01-15 15:58:57 +0800

    feat: integrate posthog to landing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { PostHogProvider } from "posthog-js/react";
 
-createRoot(document.getElementById('root')!).render(
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-11-30",
+  disabled: import.meta.env.DEV,
+} as const;
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+      <App />
+    </PostHogProvider>
   </StrictMode>
-)
+);
